f9bdbba6b407072b3fc09f2f7737206ab358c094
satoru komatsu <sk.comiu@gmail.com>
2022-04-14 18:32:31 +0900

    一方向の裏返し処理の実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,124 @@
-//ゲーム用のボード
+//ゲーム用のボードの定義
 class OthelloBoard {
     constructor(parent) {
         this.parent = document.getElementById(parent);
-        const sqArr = new Array(90);
+        this.sqArr = new Array(91);
+        this.tarn = 1;  //ターン：黒ー１、白−２
     
         for (let i = 0; i < 91; i++) {
             //ボード上のセルを作る
             const el = document.createElement('div');
             el.className = 'cell';
-            el.id = `${i}`;
             this.parent.appendChild(el);
-            sqArr[i] = el;
+            this.sqArr[i] = el;
+            console.log(this.sqArr[i])
 
             //外枠は表示なし
             if (Math.floor(i / 9) === 0 || Math.floor(i / 9) === 9 || i % 9 === 0) {
-                document.getElementById(`${i}`).style.display = 'none';
+                this.sqArr[i].style.display = 'none';
             }
 
             //最初に全てのセルを石で埋めておく
-            this.cell = document.getElementById(`${i}`);
             const disc = document.createElement('div');
-            this.cell.appendChild(disc);
             disc.className = 'disc';
-            disc.style.display = 'none';          
+            disc.style.display = 'none';
+            el.appendChild(disc);
+            el.disc = disc;
+
+            //クリックしたら反応するイベント
+            el.addEventListener('click', e => {
+                onClickBoard(`${i}`)
+            })
         }
     }
-
-    //石の表示
-    //非表示 : 0
-    //黒    : 1
-    //白    : 2
-    //ボードの外枠: 8
+    
+    //石の表示・非表示、黒・白
     setDisc(n, d) {
-        const cellNum = document.getElementById(`${n}`);
-        const disc = cellNum.firstChild;
-        if(d === 1) {
-            disc.style.display = 'block';
-            disc.style.backgroundColor = 'black';
-        } else if(d === 2) {
-            disc.style.display = 'block';
-            disc.style.backgroundColor = 'white';
+        this.sqArr[n].disc.style.display = d === 0? 'none' : 'block';
+        if (d > 0) {
+            this.sqArr[n].disc.style.backgroundColor = d === 1? 'black' : 'white';
         }
     }
 
+    //現在の盤上の数字を取得して石を表示する
+    currentDisc(discArr) {
+        for (let i = 0; i < 91; i++) {
+            this.setDisc(i, discArr.getNum(i));
+        }
+    }
+}
+
+
+
+//石の動作の定義
+class OthelloDisc {
+    constructor() {
+        this.discArr = new Array(91);
+        this.vector = [1]
+        this.turn = 1;
+
+        //石の表示
+        //非表示 　　　: 0
+        //黒    　　　: 1
+        //白    　　　: 2
+        //ボードの外枠: 8
+        for (let i = 0; i < 91; i++) {
+            if (Math.floor(i / 9) === 0 || Math.floor(i / 9) === 9 || i % 9 === 0) {
+                this.discArr[i] = 8;
+            } else {
+                this.discArr[i] = 0;
+            }
+        }
+
+        this.discArr[40] = 2;
+        this.discArr[50] = 2;
+        this.discArr[41] = 1;
+        this.discArr[49] = 1;
+    }
+
+    //配列の数字を取得するだけ
+    getNum(n) {
+        return this.discArr[n];
+    }
+
+    //石を置いて裏返す
+    moveDisc(pos) {
+        if (this.discArr[pos] !== 0) {  //既に石があったら置けない
+            alert('そこには置けません');
+            return;
+        }
+        const oppositDisc = this.turn === 2? 1 : 2;
+        for (let i = 0; i < this.vector.length; i++) {
+            const vec = this.vector[i];
+            let currentPos = parseInt(pos);
+            let n = currentPos + vec;
+            let flipp = 0;
+            if (this.discArr[n] === this.turn) {
+                alert('そこには置けません');
+                return;
+            }
+            while (this.discArr[n] === oppositDisc) {
+                n += vec;
+                flipp++;
+            }
+            if (this.discArr[n] === this.turn) {
+                for (let i = 0; i < flipp; i++) {
+                    this.discArr[n - vec] = this.turn;
+                }
+                this.discArr[pos] = this.discArr;
+            }
+        }
+    }
+    
 }
 
 const othelloBoard = new OthelloBoard('board');
+const othelloDisc = new OthelloDisc();
 
-//スタート時の石の配置　(位置, 色)
-othelloBoard.setDisc( 40,  2);
-othelloBoard.setDisc( 50,  2);
-othelloBoard.setDisc( 41,  1);
-othelloBoard.setDisc( 49,  1);
+othelloBoard.currentDisc(othelloDisc)
+
+function onClickBoard(pos) {
+    // console.log(pos)
+    othelloDisc.moveDisc(pos);
+    othelloBoard.currentDisc(othelloDisc);
+}
